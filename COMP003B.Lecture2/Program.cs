dc9ffa9061d5189cd932decf28e18f9fcf8e7f8e
isaaccrxz <157342207+isaaccrxz@users.noreply.github.com>
2024-04-01 18:37:32 -0700

    Add project files.

---
+++ COMP003B.Lecture2/Program.cs
@@ -0,0 +1,41 @@
+namespace COMP003B.Lecture2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            // middleware
+
+            // Welcome page at /Welcome
+            app.UseWelcomePage("/Welcome");
+            // added HTTP error code middleware
+            app.UseStatusCodePages();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles(); // uses wwwroot on solution explorer
+
+            app.UseRouting();
+
+            app.UseAuthorization(); // insert secrets that the user cant access
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
